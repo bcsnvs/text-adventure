ab927f3bae5a2b3638ab34fff52d9d5ab459afcf
Borja <bcg.casanovas@gmail.com>
2021-02-24 10:20:21 +0900

    Added launch config

--- adventure.js
+++ adventure.js
@@ -1,1 +1,25 @@
-console.log("Hi");
+const express = require("express");
+const fs = require("fs");
+const port = 3000;
+const Chance = require("chance");
+
+
+// App handle
+const app = express();
+
+
+app.get("/", function(request, response){
+
+
+
+
+
+
+
+});
+
+
+//Start listening
+app.listen(port, function() {
+    console.log("App listening on port " + port)
+});
